869c1eff5d87d2ac8ba778b0cf034f68946b1478
カラス <hugobenicioconrado@gmail.com>
2025-04-19 00:47:56 -0300

    Update create_schema.sql

--- create_schema.sql
+++ create_schema.sql
@@ -27,9 +27,35 @@ FOREIGN KEY (id_author) REFERENCES author(id_author),
 FOREIGN KEY (id_publisher) REFERENCES publisher(id_publisher)
 );
 
+CREATE TABLE user(
+id_user INT AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+sex CHAR(1)
+);
 
 
+CREATE TABLE book_loan(
+id_loan INT AUTO_INCREMENT PRIMARY KEY,
+id_book INT NOT NULL,
+id_user INT NOT NULL,
+dateloan DATE NOT NULL,
+datereturn DATE NOT NULL,
+FOREIGN KEY (id_book) REFERENCES book(id_book),
+FOREIGN KEY (id_user) REFERENCES user(id_user)
+);
 
+ALTER TABLE user
+add email VARCHAR(50),
+add phone_number VARCHAR(15);
 
+ALTER TABLE book
+MODIFY name VARCHAR(100);
 
+ALTER TABLE book
+RENAME COLUMN name TO book_name;
 
+ALTER TABLE author
+RENAME COLUMN name TO author_name;
+
+ALTER TABLE publisher
+RENAME COLUMN name TO publisher_name;
